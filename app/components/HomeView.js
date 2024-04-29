05d8f98a90bdb5c54e45203b19a0581120d8be30
Simon Jern <simon_jern@msn.com>
2024-04-29 15:15:07 +0200

    Snackbar and highscore
    A snackbar is now displayed when answering correctly or wrongly. highscore is stored in localstorage.
    
    Co-Authored-By: JSalomonsson <102297866+jsalomonsson@users.noreply.github.com>
    Co-Authored-By: Erik <102734798+eriklarsson19@users.noreply.github.com>

--- app/components/HomeView.js
+++ app/components/HomeView.js
@@ -10,12 +10,13 @@ import {
   List,
   ListItem,
   ListItemText,
+  Snackbar,
 } from "@mui/material";
 import { useState } from "react";
 import Lyrics from "./Lyrics";
 import getLyricsForTrackId from "../actions";
 
-export default function HomeView({ trackList, firstLyrics }) {
+export default function HomeView({ trackList, firstLyrics, currentPage }) {
   const [gameState, setGameState] = useState("off");
   const [currentLyrics, setCurrentLyrics] = useState(firstLyrics);
   const [titleInput, setTitleInput] = useState("");
@@ -25,6 +26,9 @@ export default function HomeView({ trackList, firstLyrics }) {
   const [numOfLyricsState, setNumOfLyricsState] = useState(1);
   const [error, setError] = useState();
   const [displayInstructions, setDisplayInstructions] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarSeverity, setSnackbarSeverity] = useState("success");
 
   const addLyrics = () => {
     if (numOfLyricsState < 5) {
@@ -48,26 +52,44 @@ export default function HomeView({ trackList, firstLyrics }) {
       setError("Please enter a song title");
       return;
     }
-    setError("");
 
     const trackTitle = trackList[currentTrackIndex].track.track_name;
-    const trimmedTrackTitle = trackTitle.replace(/\s/g, "");
-    const cleanedTrackTitle = trimmedTrackTitle.split("(")[0];
+    const cleanedTrackTitle = trackTitle.split("(")[0];
+    const trimmedTrackTitle = cleanedTrackTitle.replace(/\s/g, "");
 
-    if (cleanedTitleInput.toLowerCase() === cleanedTrackTitle.toLowerCase()) {
+    if (cleanedTitleInput.toLowerCase() === trimmedTrackTitle.toLowerCase()) {
       setTotalScore(totalScore + scoreForCurrentSong);
-      setCurrentTrackIndex(currentTrackIndex + 1);
-      setScoreForCurrentSong(5);
-      setTitleInput("");
-
-      await setNewLyrics();
-      setNumOfLyricsState(1);
+      setSnackbarMessage(`Correct!`);
+      setSnackbarSeverity("success");
+      setSnackbarOpen(true);
+      await nextSong();
+    } else {
+      setSnackbarMessage(
+        `Sorry, incorrect! The song was "${cleanedTrackTitle}".`
+      );
+      setSnackbarSeverity("error");
+      setSnackbarOpen(true);
+      await nextSong();
     }
   };
 
-  const skipSong = async () => {
+  const nextSong = async () => {
     if (currentTrackIndex === trackList.length - 1) {
       if (window) {
+        alert("The game has ended. Your score is: " + totalScore);
+        if (currentPage < 8) {
+          document.cookie = `page=${currentPage + 1}; path=/;`;
+        } else {
+          document.cookie = `page=1; path=/;`;
+        }
+        if (localStorage.getItem("highScore")) {
+          let currentHighScore = parseInt(localStorage.getItem("highScore"));
+          if (currentHighScore < totalScore) {
+            localStorage.setItem("highScore", totalScore.toString());
+          }
+        } else {
+          localStorage.setItem("highScore", totalScore.toString());
+        }
         window.location.reload();
       }
     } else {
@@ -82,124 +104,150 @@ export default function HomeView({ trackList, firstLyrics }) {
   };
 
   return (
-    <Box
-      display="flex"
-      flexDirection="row"
-      justifyContent="center"
-      alignContent="center"
-    >
+    <>
       <Box
-        sx={{
-          maxWidth: { xs: "95%", sm: "40%" },
-          minWidth: { xs: "95%", sm: "40%" },
-        }}
+        display="flex"
+        flexDirection="row"
+        justifyContent="center"
+        alignContent="center"
       >
-        <Typography textAlign="center" variant="h2" mb={2} mt={2}>
-          Guess the Song
-        </Typography>
-        {gameState === "on" && (
-          <Box display="flex" justifyContent="center" alignItems="center">
-            <Button
-              variant="text"
-              onClick={() => setDisplayInstructions(!displayInstructions)}
-            >
-              {displayInstructions ? "Hide Instructions" : "Show Instructions"}
-            </Button>
-          </Box>
-        )}
-        {(gameState === "off" || displayInstructions) && (
-          <Alert icon={<MusicNote />} sx={{ mb: 2, fontSize: "10px" }}>
-            How to Play:{" "}
-            <List component="nav" aria-label="game instructions">
-              <ListItem>
-                <ListItemText primary="1. Start the Game: Click 'Start Game' to display the first set of lyrics from a song." />
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="2. Guess the Song: Enter your guess for the song title based on the lyrics shown in the text field." />
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="3. Submit Your Answer: Hit 'Submit' to check if your guess is correct. You only get one chance to guess each song correctly. Earn up to 5 points for a correct answer." />
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="4. Need a Hint? Click 'Add More Lyrics' to reveal additional lyrics. Each hint reduces the potential score by 1 point, down to a minimum of 1 point." />
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="5. Skip: If you decide not to guess, click 'Skip' to move to the next song without earning points." />
-              </ListItem>
-            </List>
-          </Alert>
-        )}
-
-        {gameState === "on" && (
-          <Alert icon={false} severity="info">
-            <Typography mt={1} variant="body1">
-              Current track: {currentTrackIndex + 1} / {trackList.length}
-            </Typography>
-            <Typography mt={1} variant="body1">
-              Total score: {totalScore}
-            </Typography>
-          </Alert>
-        )}
-
-        {gameState === "off" ? (
-          <Box display="flex" justifyContent="center" alignItems="center">
-            <Button
-              onClick={() => {
-                setGameState("on");
-              }}
-              variant="contained"
-              size="large"
-              sx={{
-                mt: 1,
-                mb: 2,
-              }}
-            >
-              Start Game
-            </Button>
-          </Box>
-        ) : (
-          <Box>
-            <Box
-              mt={2}
-              display="flex"
-              flexDirection="column"
-              alignItems="flex-end"
-            >
-              <TextField
-                fullWidth={true}
-                id="filled-basic"
-                label="Your Guess"
-                variant="filled"
-                value={titleInput}
-                onChange={(e) => setTitleInput(e.target.value)}
-                error={Boolean(error)}
-                helperText={error}
-              />
-              <Box>
-                <Button
-                  onClick={skipSong}
-                  sx={{ width: "fit-content", mt: 1, mr: 1 }}
-                  variant="outlined"
-                >
-                  Skip
-                </Button>
-                <Button
-                  onClick={checkAnswer}
-                  sx={{ width: "fit-content", mt: 1 }}
-                  variant="contained"
-                >
-                  Submit (+{scoreForCurrentSong} points)
-                </Button>
-              </Box>
+        <Box
+          sx={{
+            maxWidth: { xs: "95%", sm: "40%" },
+            minWidth: { xs: "95%", sm: "40%" },
+          }}
+        >
+          <Typography textAlign="center" variant="h2" mb={2} mt={2}>
+            Guess the Song
+          </Typography>
+          {gameState === "on" && (
+            <Box display="flex" justifyContent="center" alignItems="center">
+              <Button
+                variant="text"
+                onClick={() => setDisplayInstructions(!displayInstructions)}
+              >
+                {displayInstructions
+                  ? "Hide Instructions"
+                  : "Show Instructions"}
+              </Button>
             </Box>
-            <Lyrics
-              lyrics={currentLyrics.filter((lyric) => lyric !== "")}
-              numOfLyricsState={numOfLyricsState}
-              addLyrics={addLyrics}
-            />
-          </Box>
-        )}
+          )}
+          {(gameState === "off" || displayInstructions) && (
+            <Alert icon={<MusicNote />} sx={{ mb: 2, fontSize: "10px" }}>
+              How to Play:{" "}
+              <List component="nav" aria-label="game instructions">
+                <ListItem>
+                  <ListItemText primary="1. Start the Game: Click 'Start Game' to display the first set of lyrics from a song." />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="2. Guess the Song: Enter your guess for the song title based on the lyrics shown in the text field." />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="3. Submit Your Answer: Hit 'Submit' to check if your guess is correct. You only get one chance to guess each song correctly. Earn up to 5 points for a correct answer." />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="4. Need a Hint? Click 'Add More Lyrics' to reveal additional lyrics. Each hint reduces the potential score by 1 point, down to a minimum of 1 point." />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="5. Skip: If you decide not to guess, click 'Skip' to move to the next song without earning points." />
+                </ListItem>
+              </List>
+            </Alert>
+          )}
+
+          {gameState === "on" && (
+            <Alert icon={false} severity="info">
+              <Typography mt={1} variant="body1">
+                Current track: {currentTrackIndex + 1} / {trackList.length}
+              </Typography>
+              <Typography mt={1} variant="body1">
+                Current score: {totalScore} / 50
+              </Typography>
+              <Typography mt={1} variant="body1">
+                Highscore:{" "}
+                {localStorage.getItem("highScore")
+                  ? localStorage.getItem("highScore")
+                  : 0}{" "}
+                / 50
+              </Typography>
+            </Alert>
+          )}
+
+          {gameState === "off" ? (
+            <Box display="flex" justifyContent="center" alignItems="center">
+              <Button
+                onClick={() => {
+                  setGameState("on");
+                }}
+                variant="contained"
+                size="large"
+                sx={{
+                  mt: 1,
+                  mb: 2,
+                }}
+              >
+                Start Game
+              </Button>
+            </Box>
+          ) : (
+            <Box>
+              <Box
+                mt={2}
+                display="flex"
+                flexDirection="column"
+                alignItems="flex-end"
+              >
+                <TextField
+                  fullWidth={true}
+                  id="filled-basic"
+                  label="Your Guess"
+                  variant="filled"
+                  value={titleInput}
+                  onChange={(e) => setTitleInput(e.target.value)}
+                  error={Boolean(error)}
+                  helperText={error}
+                />
+                <Box>
+                  <Button
+                    onClick={nextSong}
+                    sx={{ width: "fit-content", mt: 1, mr: 1 }}
+                    variant="outlined"
+                  >
+                    Skip
+                  </Button>
+                  <Button
+                    onClick={checkAnswer}
+                    sx={{ width: "fit-content", mt: 1 }}
+                    variant="contained"
+                  >
+                    Submit (+{scoreForCurrentSong} points)
+                  </Button>
+                </Box>
+              </Box>
+              <Lyrics
+                lyrics={currentLyrics.filter((lyric) => lyric !== "")}
+                numOfLyricsState={numOfLyricsState}
+                addLyrics={addLyrics}
+              />
+            </Box>
+          )}
+        </Box>
       </Box>
-    </Box>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        onClose={() => setSnackbarOpen(false)}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+      >
+        <Alert
+          onClose={() => setSnackbarOpen(false)}
+          severity={snackbarSeverity}
+          variant="filled"
+          sx={{ width: "100%" }}
+        >
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
+import { cookies } from "next/headers.js";
 import getLyricsForTrackId from "./actions.js";
 import HomeView from "./components/HomeView.js";
 
-async function getTopTracks(country) {
+async function getTopTracks(country, page) {
   const response = await fetch(
-    `https://api.musixmatch.com/ws/1.1/chart.tracks.get?apikey=e897c43680faf0f22debd33edab6ae34&chart_name=top&page=1&page_size=10&country=${country}&f_has_lyrics=1`,
+    `https://api.musixmatch.com/ws/1.1/chart.tracks.get?apikey=e897c43680faf0f22debd33edab6ae34&chart_name=top&page=${page}&page_size=10&country=${country}&f_has_lyrics=1`,
     {
       method: "GET",
     }
@@ -14,12 +15,21 @@ async function getTopTracks(country) {
 }
 
 export default async function Home({ searchParams }) {
+  const cookieStore = cookies();
+  const page = cookieStore.get("page")?.value;
+  const pageAsNumber = parseInt(page, 10) || 1;
   const country = searchParams?.country || "US";
-  const trackList = await getTopTracks(country);
+  const trackList = await getTopTracks(country, page);
 
   const firstLyrics = await getLyricsForTrackId(trackList[0].track.track_id);
   const lyricsInRows = firstLyrics.split("\n");
 
   if (firstLyrics)
-    return <HomeView trackList={trackList} firstLyrics={lyricsInRows} />;
+    return (
+      <HomeView
+        trackList={trackList}
+        firstLyrics={lyricsInRows}
+        currentPage={pageAsNumber}
+      />
+    );
 }
